ec233f9d303b321f70085635f8dd990e28d47b3d
neerajadhav <adhavneeraj9500@gmail.com>
2021-11-09 20:10:23 +0000

    deploy: neerajadhav/neerajadhav-source@80369604a110752b56331d9e1f84dca1be2fa52e

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(['/', '/author/','/author/neeraj/','/archive/','/column/']);
+            return cache.addAll(['/', '/author/','/author/neeraj/','/tags/','/series/']);
         })
     );
 });
