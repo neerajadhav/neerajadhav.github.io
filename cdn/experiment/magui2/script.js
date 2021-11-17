68184b921b0d0a332908e7fa509443e408b86bbe
neerajadhav <adhavneeraj9500@gmail.com>
2021-11-17 07:15:23 +0000

    deploy: neerajadhav/neerajadhav-source@2943bf95a7c6f45f8fddc9e8e014cad1edd23773

--- cdn/experiment/magui2/script.js
+++ cdn/experiment/magui2/script.js
@@ -10,7 +10,7 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky","shadow");
+    header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
