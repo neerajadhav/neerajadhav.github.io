856cb2021523a9496a6ab8ebb4d1fed271e54204
neerajadhav <adhavneeraj9500@gmail.com>
2022-07-27 20:27:02 +0000

    deploy: neerajadhav/neerajadhav-source@5f3ab4414fbf7cf7b2c72f78a0a56c2c8a18f503

---
+++ eseerooms/js/main.js
@@ -0,0 +1,29 @@
+var sideMenu = document.getElementById('side-menu');
+var openIcon = document.getElementById('openIcon');
+var closeIcon = document.getElementById('closeIcon');
+var overlay = document.getElementById('overlay');
+var header = document.getElementById('header');
+
+function openMenu() {
+    sideMenu.classList.remove('right-[-250px]');
+    sideMenu.classList.remove('top-[-250px]');
+    sideMenu.classList.add('right-[15px]');
+    sideMenu.classList.add('top-[80px]');
+    openIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    header.classList.remove('md:bg-white');
+    // header.classList.add('bg-white');
+}
+
+function closeMenu() {
+    sideMenu.classList.remove('right-[15px]');
+    sideMenu.classList.remove('top-[80px]');
+    sideMenu.classList.add('right-[-250px]');
+    sideMenu.classList.add('top-[-250px]');
+    openIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+    overlay.classList.add('hidden');
+    header.classList.add('md:bg-white');
+    // header.classList.remove('bg-white');
+}
