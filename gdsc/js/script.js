a800d50f8c4229ae042fab1acebb0b209b6d0d1e
neerajadhav <adhavneeraj9500@gmail.com>
2022-11-02 04:05:42 +0000

    deploy: neerajadhav/neerajadhav-source@9c41ab671a80c509449df0bf53a8e265d86571fc

--- gdsc/js/script.js
+++ gdsc/js/script.js
@@ -29,8 +29,8 @@ function rightopen() {
     var overlay = document.getElementById("rightoverlay");
     var body = document.getElementsByTagName("BODY")[0];
 
-    left.classList.remove("right-[-300px]");
-    left.classList.add("right-0");
+    left.classList.remove("left-[-300px]");
+    left.classList.add("left-0");
 
     overlay.classList.remove("hidden");
     body.classList.add("fixed");
@@ -42,8 +42,8 @@ function rightover() {
     var overlay = document.getElementById("rightoverlay");
     var body = document.getElementsByTagName("BODY")[0];
 
-    left.classList.add("right-[-300px]");
-    left.classList.remove("right-0");
+    left.classList.add("left-[-300px]");
+    left.classList.remove("left-0");
 
     overlay.classList.add("hidden");
     body.classList.remove("fixed");
